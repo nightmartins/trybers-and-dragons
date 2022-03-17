1b4af7877579a7921f5b8823a764d4e6f97dd9cc
natalia <mellomartins.naty@gmail.com>
2022-03-17 17:46:27 -0300

    requisito 3

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,27 @@
+export default abstract class Archetype {
+  private _name: string;
+  private _special: number;
+  private _cost: number;
+
+  constructor(name: string, special: number, cost: number) {
+    this._name = name;
+    this._special = special;
+    this._cost = cost;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get special(): number {
+    return this._special;
+  }
+
+  get cost(): number {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+}
